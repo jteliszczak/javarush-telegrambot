e44c1d48f72c5c5c2254b7862ec4c0661302ce73
jteliszczak <jurijteliszczak@gmail.com>
2021-10-09 18:26:42 +0300

    JRTB-0: added SpringBoot skeleton to the project

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavarushTelegramBotApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegramBotApplication.class, args);
 	}
