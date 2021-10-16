fcedad824ec4ca5c4955a6c745e629d51d22dc7a
jteliszczak <jurijteliszczak@gmail.com>
2021-10-16 17:08:56 +0300

    JRTB-3: added Command pattern for handling Telegram Bot commands

---
+++ src/test/java/com/github/javarushcommunity/jrtb/command/StartCommandTest.java
@@ -0,0 +1,26 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.javarushcommunity.jrtb.command.CommandName.*;
+import static com.github.javarushcommunity.jrtb.command.StartCommand.*;
+
+@DisplayName("Unit-level testing for StartCommand")
+public class StartCommandTest extends AbstractCommandTest {
+
+    @Override
+    String getCommandName() {
+        return START.getCommandName();
+    }
+
+    @Override
+    String getCommandMessage() {
+        return START_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new StartCommand(sendBotMessageService);
+    }
+
+}
---
+++ src/test/java/com/github/javarushcommunity/jrtb/command/UnknownCommandTest.java
@@ -0,0 +1,25 @@
+package com.github.javarushcommunity.jrtb.command;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static com.github.javarushcommunity.jrtb.command.CommandName.*;
+import static com.github.javarushcommunity.jrtb.command.UnknownCommand.*;
+
+@DisplayName("Unit-level testing for UnknownCommand")
+public class UnknownCommandTest extends AbstractCommandTest {
+
+    @Override
+    String getCommandName() {
+        return "/fdgdfgdfgdbd";
+    }
+
+    @Override
+    String getCommandMessage() {
+        return UNKNOWN_MESSAGE;
+    }
+
+    @Override
+    Command getCommand() {
+        return new UnknownCommand(sendBotMessageService);
+    }
+}
